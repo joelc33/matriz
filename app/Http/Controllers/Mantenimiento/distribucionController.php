36a091312482f4225ef8a9dff65f3f6b53ac079f
Yoser Perez <yoserp1@gmail.com>
2017-12-05 09:22:32 -0400

    Distribucion de municipios

--- app/Http/Controllers/Mantenimiento/distribucionController.php
+++ app/Http/Controllers/Mantenimiento/distribucionController.php
@@ -8,6 +8,7 @@ use Validator;
 use Input;
 use Response;
 use DB;
+use Session;
 //*******************************//
 use Illuminate\Http\Request;
 
@@ -47,15 +48,16 @@ class distribucionController extends Controller
       $variable = Input::get('variable');
 
       $tab_distribucion_municipio = $this->tab_distribucion_municipio
-      ->join('mantenimiento.tab_tipo_personal as t01','t01.id','=','mantenimiento.tab_distribucion_municipio.id_tab_tipo_personal')
-      ->select( 'mantenimiento.tab_distribucion_municipio.id', 'id_tab_ejercicio_fiscal', 'id_tab_tipo_personal', 'nu_masculino',
-       'nu_femenino', 'mo_sueldo', 'mo_compensacion', 'mo_primas',
-       'mantenimiento.tab_distribucion_municipio.in_activo', 'nu_codigo', 'de_tipo_personal' );
+      ->join('mantenimiento.tab_municipio as t01','t01.id','=','mantenimiento.tab_distribucion_municipio.id_tab_municipio')
+      ->select( 'mantenimiento.tab_distribucion_municipio.id', 'id_tab_ejercicio_fiscal', 'id_tab_municipio', 'co_partida', 'nu_base_censo',
+       'nu_factor_poblacion', 'cuatrocinco_ppi', 'cincocero_fpp', 'superficie_km',
+       'superficie_factor', 'extension_territorio', 'mo_total', 'de_municipio' )
+       ->where('id_tab_ejercicio_fiscal', '=', Session::get('ejercicio'));
 
       if (Input::get("BuscarBy")=="true") {
 
         if($variable!=""){
-          $tab_distribucion_municipio->where('de_tipo_personal', 'ILIKE', "%$variable%");
+          $tab_distribucion_municipio->where('de_municipio', 'ILIKE', "%$variable%");
         }
 
         $response['success']  = 'true';
@@ -71,7 +73,34 @@ class distribucionController extends Controller
 
       return Response::json($response, 200);
     } catch (\Illuminate\Database\QueryException $e) {
-      return Response::json(array('success' => false, 'message' => utf8_encode( $e->getMessage())), 200);
+      return Response::json(array('success' => false, 'message' => utf8_encode( $e->getMessage())), 500);
     }
   }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function nuevo()
+  {
+    $data = json_encode(array("id" => ""));
+    return View::make('mantenimiento.distribucion.editar')->with('data',$data);
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function editar($id)
+  {
+    $data = tab_distribucion_municipio::select('id', 'id_tab_ejercicio_fiscal', 'id_tab_municipio', 'co_partida', 'nu_base_censo',
+       'nu_factor_poblacion', 'cuatrocinco_ppi', 'cincocero_fpp', 'superficie_km',
+       'superficie_factor', 'extension_territorio', 'mo_total')
+    ->where('id', '=', $id)
+    ->first();
+    return View::make('mantenimiento.distribucion.editar')->with('data',$data);
+  }
+
 }
--- app/Models/Mantenimiento/tab_distribucion_municipio.php
+++ app/Models/Mantenimiento/tab_distribucion_municipio.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class tab_distribucion_municipio extends Model
 {
-    //
+  //Nombre de la conexion que utitlizara este modelo
+	protected $connection= 'local';
+
+	//Todos los modelos deben extender la clase Eloquent
+	protected $table = 'mantenimiento.tab_distribucion_municipio';
+
+	public static $validarCrear = array(
+		"cargo" => "required|min:1|max:1200|unique:tab_cargo,de_cargo"
+	);
+
+	public static $validarEditar = array(
+		"cargo" => "required|min:1|max:1200"
+	);
 }
