35b1a3f1a37c48101cb35b9d00ff06805e885914
Yoser Perez <yoserp1@gmail.com>
2017-12-22 07:54:23 -0400

    Forma 004 AC

--- app/Http/Controllers/AcSeguimiento/formacuatroController.php
+++ app/Http/Controllers/AcSeguimiento/formacuatroController.php
@@ -1,7 +1,18 @@
 <?php
 
 namespace matriz\Http\Controllers\AcSeguimiento;
-
+//*******agregar esta linea******//
+use matriz\Models\AcSegto\tab_ac;
+use matriz\Models\AcSegto\tab_ac_ae;
+use matriz\Models\AcSegto\tab_meta_fisica;
+use matriz\Models\AcSegto\tab_meta_financiera;
+use View;
+use Validator;
+use Input;
+use Response;
+use DB;
+use Session;
+//*******************************//
 use Illuminate\Http\Request;
 
 use matriz\Http\Requests;
@@ -9,79 +20,84 @@ use matriz\Http\Controllers\Controller;
 
 class formacuatroController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+  protected $tab_ac;
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  public function __construct(tab_ac $tab_ac)
+  {
+    $this->middleware('auth');
+    $this->tab_ac = $tab_ac;
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+  * Display a listing of the resource.
+  *
+  * @return Response
+  */
+  public function lista()
+  {
+    return View::make('seguimiento.ac.004.lista');
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Response
+   */
+  public function storeLista()
+  {
+    try {
+      $start  = Input::get('start', 0);
+      $limit  = Input::get('limit', 20);
+      $variable = Input::get('variable');
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+      $tab_ac = $this->tab_ac
+      ->join('mantenimiento.tab_ejecutores as t01', 'ac_seguimiento.tab_ac.id_tab_ejecutores', '=', 't01.id_ejecutor')
+      ->join('mantenimiento.tab_lapso as t02', 'ac_seguimiento.tab_ac.id_tab_lapso', '=', 't02.id')
+      ->select( 'ac_seguimiento.tab_ac.id', 'tx_ejecutor', 'ac_seguimiento.tab_ac.id_tab_ejecutores',
+      'ac_seguimiento.tab_ac.in_activo',
+      DB::raw("to_char(t02.fe_inicio, 'dd/mm/YYYY') as fe_inicio"),
+      DB::raw("to_char(t02.fe_fin, 'dd/mm/YYYY') as fe_fin"), 'nu_codigo', 'de_ac' );
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+      if (Input::get("BuscarBy")=="true") {
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        if($variable!=""){
+          $tab_ac->where('de_aplicacion', 'ILIKE', "%$variable%");
+        }
+
+        $response['success']  = 'true';
+        $response['total'] = $tab_ac->count();
+        $tab_ac->skip($start)->take($limit);
+        $response['data']  = $tab_ac->orderby('ac_seguimiento.tab_ac.id','ASC')->get()->toArray();
+      } else {
+        $response['success']  = 'true';
+        $response['total'] = $tab_ac->count();
+        $tab_ac->skip($start)->take($limit);
+        $response['data']  = $tab_ac->orderby('ac_seguimiento.tab_ac.id','ASC')->get()->toArray();
+      }
+
+      return Response::json($response, 200);
+    } catch (\Illuminate\Database\QueryException $e) {
+      return Response::json(array('success' => false, 'message' => utf8_encode( $e->getMessage())), 200);
     }
+  }
+
+  /**
+  * Display a listing of the resource.
+  *
+  * @return Response
+  */
+  public function detalle()
+  {
+    $data = tab_ac::join('mantenimiento.tab_ejecutores as t01', 'ac_seguimiento.tab_ac.id_tab_ejecutores', '=', 't01.id_ejecutor')
+    ->join('mantenimiento.tab_lapso as t02', 'ac_seguimiento.tab_ac.id_tab_lapso', '=', 't02.id')
+    ->select( 'ac_seguimiento.tab_ac.id', 'tx_ejecutor', 'ac_seguimiento.tab_ac.id_tab_ejecutores',
+    'ac_seguimiento.tab_ac.in_activo',
+    DB::raw("to_char(t02.fe_inicio, 'dd/mm/YYYY') as fe_inicio"),
+    DB::raw("to_char(t02.fe_fin, 'dd/mm/YYYY') as fe_fin"), 'nu_codigo', 'de_ac' )
+    ->where('ac_seguimiento.tab_ac.id', '=', Input::get('codigo'))
+    ->first();
+
+    return View::make('seguimiento.ac.004.detalle')->with('data',$data);
+  }
+
 }
---
+++ resources/views/seguimiento/ac/004/lista.blade.php
@@ -0,0 +1,303 @@
+<script type="text/javascript">
+Ext.ns("forma004Lista");
+function change(val){
+	if(val==true){
+	    return '<span style="color:green;">Activo</span>';
+	}else if(val==false){
+	    return '<span style="color:red;">Inactivo</span>';
+	}
+return val;
+};
+function movimiento(val){
+	if(val==true){
+	    return '<span style="color:green;">Si</span>';
+	}else if(val==false){
+	    return '<span style="color:red;">No</span>';
+	}
+return val;
+};
+forma004Lista.main = {
+condicion:function(codigo){
+    return (codigo=='0')?'NO':'SI';
+},
+init:function(){
+//Mascara general del modulo
+this.mascara = new Ext.LoadMask(Ext.getBody(), {msg:"Cargando..."});
+
+//objeto store
+this.store_lista = this.getLista();
+
+//Agregar un registro
+this.nuevo = new Ext.Button({
+    text:'Nuevo',
+    iconCls: 'icon-nuevo',
+    handler:function(){
+        forma004Lista.main.mascara.show();
+        this.msg = Ext.get('formularioacseguimiento');
+        this.msg.load({
+         url:"{{ URL::to('ac/seguimiento/nuevo') }}",
+         scripts: true,
+         text: "Cargando.."
+        });
+    }
+});
+
+//Editar un registro
+this.editar= new Ext.Button({
+    text:'Ver Ficha',
+    iconCls: 'icon-pdf',
+    handler:function(){
+	this.codigo  = forma004Lista.main.gridPanel_.getSelectionModel().getSelected().get('id');
+	forma004Lista.main.mascara.show();
+        this.msg = Ext.get('formularioacseguimiento');
+        this.msg.load({
+         url:"{{ URL::to('ac/seguimiento/editar') }}/"+this.codigo,
+         scripts: true,
+         text: "Cargando.."
+        });
+    }
+});
+
+//Desabilitar un registro
+this.eliminar= new Ext.Button({
+    text:'Deshabilitar',
+    iconCls: 'icon-cancelar',
+    handler:function(){
+	this.codigo  = forma004Lista.main.gridPanel_.getSelectionModel().getSelected().get('id');
+	Ext.MessageBox.confirm('Confirmación', '¿Realmente desea Deshabilitar Registro?', function(boton){
+	if(boton=="yes"){
+        Ext.Ajax.request({
+            method:'POST',
+            url:'{{ URL::to('seguimiento/ac/eliminar') }}',
+            params:{
+		_token: '{{ csrf_token() }}',
+                id: forma004Lista.main.gridPanel_.getSelectionModel().getSelected().get('id')
+            },
+            success:function(result, request ) {
+                obj = Ext.util.JSON.decode(result.responseText);
+                if(obj.success=="true"){
+		    forma004Lista.main.store_lista.load();
+                    Ext.Msg.alert("Notificación",obj.msg);
+                }else{
+                    Ext.Msg.alert("Notificación",obj.msg);
+                }
+                forma004Lista.main.mascara.hide();
+            }});
+	}});
+    }
+});
+
+this.habilitar= new Ext.Button({
+    text:'Habilitar',
+    iconCls: 'icon-fin',
+    handler:function(){
+	this.codigo  = forma004Lista.main.gridPanel_.getSelectionModel().getSelected().get('id');
+	Ext.MessageBox.confirm('Confirmación', '¿Realmente desea Habilitar Registro?', function(boton){
+	if(boton=="yes"){
+        Ext.Ajax.request({
+            method:'POST',
+            url:'{{ URL::to('seguimiento/ac/habilitar') }}',
+            params:{
+		_token: '{{ csrf_token() }}',
+                id: forma004Lista.main.gridPanel_.getSelectionModel().getSelected().get('id')
+            },
+            success:function(result, request ) {
+                obj = Ext.util.JSON.decode(result.responseText);
+                if(obj.success=="true"){
+		    forma004Lista.main.store_lista.load();
+                    Ext.Msg.alert("Notificación",obj.msg);
+                }else{
+                    Ext.Msg.alert("Notificación",obj.msg);
+                }
+                forma004Lista.main.mascara.hide();
+            }});
+	}});
+    }
+});
+
+this.editar.disable();
+this.eliminar.disable();
+this.habilitar.disable();
+
+this.buscador = new Ext.form.TwinTriggerField({
+	initComponent : function(){
+		Ext.ux.form.SearchField.superclass.initComponent.call(this);
+		this.on('specialkey', function(f, e){
+			if(e.getKey() == e.ENTER){
+				this.onTrigger2Click();
+			}
+		}, this);
+	},
+	xtype: 'twintriggerfield',
+	trigger1Class: 'x-form-clear-trigger',
+	trigger2Class: 'x-form-search-trigger',
+	enableKeyEvents : true,
+	validationEvent:false,
+	validateOnBlur:false,
+	emptyText: 'Campo de Filtro',
+	width:350,
+	hasSearch : false,
+	paramName : 'variable',
+	onTrigger1Click : function() {
+		if (this.hiddenField) {
+			this.hiddenField.value = '';
+		}
+		this.setRawValue('');
+		this.lastSelectionText = '';
+		this.applyEmptyText();
+		this.value = '';
+		this.fireEvent('clear', this);
+		forma004Lista.main.store_lista.baseParams={};
+		forma004Lista.main.store_lista.baseParams.paginar = 'si';
+		forma004Lista.main.store_lista.baseParams._token = '{{ csrf_token() }}';
+		forma004Lista.main.store_lista.load();
+	},
+	onTrigger2Click : function(){
+		var v = this.getRawValue();
+		if(v.length < 1){
+			    Ext.MessageBox.show({
+				       title: 'Notificación',
+				       msg: 'Debe ingresar un parametro de busqueda',
+				       buttons: Ext.MessageBox.OK,
+				       icon: Ext.MessageBox.WARNING
+			    });
+		}else{
+			forma004Lista.main.store_lista.baseParams={}
+			forma004Lista.main.store_lista.baseParams.BuscarBy = true;
+			forma004Lista.main.store_lista.baseParams._token = '{{ csrf_token() }}';
+			forma004Lista.main.store_lista.baseParams[this.paramName] = v;
+			forma004Lista.main.store_lista.baseParams.paginar = 'si';
+			forma004Lista.main.store_lista.load();
+		}
+	}
+});
+
+//Grid principal
+this.gridPanel_ = new Ext.grid.GridPanel({
+    iconCls: 'icon-libro',
+    store: this.store_lista,
+    border:true,
+    loadMask:true,
+    autoWidth: true,
+    autoHeight:true,
+    tbar:[
+			@if( in_array( array( 'de_privilegio' => 'acseguimiento.nuevo', 'in_habilitado' => true), Session::get('credencial') ))
+			  this.editar,'-',
+			@endif
+				this.buscador
+    ],
+    columns: [
+    new Ext.grid.RowNumberer(),
+    {header: 'id',hidden:true, menuDisabled:true,dataIndex: 'id'},
+		{header: 'Periodo', width:150,  menuDisabled:true, sortable: true, renderer: textoLargo, dataIndex: 'periodo'},
+    {header: 'Ejecutor', width:200,  menuDisabled:true, sortable: true, renderer: textoLargo, dataIndex: 'ejecutor'},
+		{header: 'Codigo', width:120,  menuDisabled:true, sortable: true, renderer: textoLargo, dataIndex: 'nu_codigo'},
+    {header: 'Descripcion', width:200,  menuDisabled:true, sortable: true, renderer: textoLargo, dataIndex: 'de_ac'},
+    {header: 'Estatus', width:80,  menuDisabled:true, sortable: true, renderer: change, dataIndex: 'in_activo'},
+    ],
+    stripeRows: true,
+    autoScroll:true,
+    stateful: true,
+    listeners:{cellclick:function(Grid, rowIndex, columnIndex,e ){
+			forma004Lista.main.editar.enable();
+			forma004Lista.main.habilitar.enable();
+			forma004Lista.main.eliminar.enable();
+		}},
+    bbar: new Ext.PagingToolbar({
+        pageSize: 20,
+        store: this.store_lista,
+        displayInfo: true,
+        displayMsg: '<span style="color:black">Registros: {0} - {1} de {2}</span>',
+        emptyMsg: "<span style=\"color:black\">No se encontraron registros</span>"
+    }),
+		sm: new Ext.grid.RowSelectionModel({
+			singleSelect: true,
+			/*AQUI ES DONDE ESTA EL LISTENER*/
+				listeners: {
+				rowselect: function(sm, row, rec) {
+					var msg = Ext.get('detalle');
+					msg.load({
+									url: '{{ URL::to('ac/seguimiento/004/detalle') }}',
+									scripts: true,
+									params: {_token:'{{ csrf_token() }}', codigo:rec.json.id},
+									text: 'Cargando...'
+					});
+					if(panel_detalle.collapsed == true)
+					{
+						panel_detalle.toggleCollapse();
+					}
+				}
+			}
+		})
+});
+
+/*Evento Doble Click*/
+this.gridPanel_.on('rowdblclick', function( grid, row, evt){
+	panel_detalle.toggleCollapse(true);
+	this.record = forma004Lista.main.store_lista.getAt(row);
+	this.codigo = this.record.data["id"];
+	this.msg = Ext.get('detalle');
+	this.msg.load({
+	    url: '{{ URL::to('ac/seguimiento/004/detalle') }}',
+	    scripts: true,
+	    params: {_token:'{{ csrf_token() }}', codigo:this.codigo},
+	    text: "Cargando..."
+	});
+});
+
+this.panel = new Ext.Panel({
+	layout: "fit",
+	border: false,
+	padding: 5,
+	items: [
+		this.gridPanel_
+	]
+});
+
+this.panel.render("contenedorforma004Lista");
+
+//Cargar el grid
+this.store_lista.baseParams.paginar = 'si';
+this.store_lista.baseParams._token = '{{ csrf_token() }}';
+this.store_lista.load();
+this.store_lista.on('load',function(){
+forma004Lista.main.editar.disable();
+forma004Lista.main.habilitar.disable();
+forma004Lista.main.eliminar.disable();
+});
+this.store_lista.on('beforeload',function(){
+panel_detalle.collapse();
+});
+},
+getLista: function(){
+    this.store = new Ext.data.JsonStore({
+	    url:'{{ URL::to('ac/seguimiento/004/storeLista') }}',
+	    root:'data',
+	    fields:[
+		    {name: 'id'},
+				{name: 'id_tab_ejecutores'},
+				{name: 'id_tab_ejecutores'},
+		    {name: 'tx_ejecutor'},
+				{name: 'nu_codigo'},
+		    {name: 'de_ac'},
+				{
+						name: 'ejecutor',
+						convert: function(v, r) {
+								return r.id_tab_ejecutores + ' - ' + r.tx_ejecutor;
+						}
+				},
+				{
+						name: 'periodo',
+						convert: function(v, r) {
+								return r.fe_inicio + ' - ' + r.fe_fin;
+						}
+				}
+	    ]
+    });
+    return this.store;
+}
+};
+Ext.onReady(forma004Lista.main.init, forma004Lista.main);
+</script>
+<div id="contenedorforma004Lista"></div>
+<div id="formularioacseguimiento"></div>
