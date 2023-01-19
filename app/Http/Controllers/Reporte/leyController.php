75c0dcfa87bdac6bd955c1c438fa8bf8937f9484
ISILI <ISILI@isilio>
2023-01-19 17:02:23 -0400

    cambios varios

--- app/Http/Controllers/Reporte/leyController.php
+++ app/Http/Controllers/Reporte/leyController.php
@@ -3771,8 +3771,8 @@ class leyController extends Controller
             ->where('nu_original', '=', $value_transferencia_dos->nu_original)
             ->where('t05.co_partida', '=', $value_transferencia_tres->co_partida)
             ->where('id_tab_tipo_ejecutor', '=', 1) 
-            ->groupBy('t06.co_partida')       
-            ->groupBy('np_uno')
+            ->groupBy('t06.co_partida')
+            ->groupBy('np_dos')        
             ->orderBy('t06.co_partida','ASC')
             ->get();
 
