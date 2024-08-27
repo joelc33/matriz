bd24cb926f8c1f394d831816997754f910b9fff1
ISILI <ISILI@isilio>
2024-08-27 10:54:46 -0400

    cambios varios

--- app/Http/Controllers/Reporte/acseguimiento004Controller.php
+++ app/Http/Controllers/Reporte/acseguimiento004Controller.php
@@ -351,7 +351,7 @@ $html23.='
                  }else{
 		$html23.='
 		<tr style="font-size:6px" nobr="true">
-		<td style="width: 100%;"  nobr="true">CAUSAS DEL DESVIO: '.$de_desvio.'</td>';
+		<td style="width: 100%;"  nobr="true" rowspan="1">CAUSAS DEL DESVIO: '.$de_desvio.'</td>';
                 $html23.='</tr>';                        
                  }
                     
@@ -375,7 +375,7 @@ $html23.='
       
         $html23.='
         <tr style="font-size:6px" nobr="true">
-        <td style="width: 100%;"  nobr="true">CAUSAS DEL DESVIO: '.$de_desvio.'</td>';
+        <td style="width: 100%;"  nobr="true" rowspan="1">CAUSAS DEL DESVIO: '.$de_desvio.'</td>';
         $html23.='</tr>';  
 
 $html23.='
@@ -634,7 +634,7 @@ $html23.='
                  }else{
 		$html23.='
 		<tr style="font-size:6px" nobr="true">
-		<td style="width: 100%;"  nobr="true">CAUSAS DEL DESVIO: '.$item->de_desvio.'</td>';
+		<td style="width: 100%;"  nobr="true" rowspan="1">CAUSAS DEL DESVIO: '.$de_desvio.'</td>';
                 $html23.='</tr>';                        
                  }
                     
@@ -658,7 +658,7 @@ $html23.='
       
         $html23.='
         <tr style="font-size:6px" nobr="true">
-        <td style="width: 100%;"  nobr="true">CAUSAS DEL DESVIO: '.$de_desvio.'</td>';
+        <td style="width: 100%;"  nobr="true" rowspan="1">CAUSAS DEL DESVIO: '.$de_desvio.'</td>';
         $html23.='</tr>';  
 
 $html23.='
