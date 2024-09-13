2ba740cece2f98274c3af651328d01b213271db7
ISILI <ISILI@isilio>
2024-09-13 14:05:09 -0400

    cambios varios

--- app/Http/Controllers/Reporte/acseguimiento004Controller.php
+++ app/Http/Controllers/Reporte/acseguimiento004Controller.php
@@ -147,6 +147,7 @@ class acseguimiento004Controller extends Controller
             })
             ->leftjoin('t45_planes_zulia as t45b', function ($join) {
             $join->on('t49.co_ambito_estado', '=', 't45b.co_ambito_zulia')
+            ->on('t49.co_objetivo_estado', '=', 't45b.co_objetivo_zulia')
             ->on('t49.co_macroproblema', '=', 't45b.co_macroproblema')
             ->on('t45b.edo_reg', '=', DB::raw('true'))        
             ->on('t45b.nu_nivel', '=', DB::raw('3'));
@@ -455,6 +456,7 @@ $html23.='
             })
             ->leftjoin('t45_planes_zulia as t45b', function ($join) {
             $join->on('t49.co_ambito_estado', '=', 't45b.co_ambito_zulia')
+            ->on('t49.co_objetivo_estado', '=', 't45b.co_objetivo_zulia')
             ->on('t49.co_macroproblema', '=', 't45b.co_macroproblema')
             ->on('t45b.edo_reg', '=', DB::raw('true'))        
             ->on('t45b.nu_nivel', '=', DB::raw('3'));
