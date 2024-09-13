e4ea13cc8669cd4d445fd4ac73771cf56adb6c00
ISILI <ISILI@isilio>
2024-09-13 10:29:23 -0400

    cambios varios

--- app/Http/Controllers/AcSeguimiento/formatresController.php
+++ app/Http/Controllers/AcSeguimiento/formatresController.php
@@ -668,7 +668,7 @@ class formatresController extends Controller
                 
                 if((round($data2->mo_causado+Input::get("causado"),2))>(round($data2->mo_comprometido+Input::get("comprometido"),0))){
                 
-                    var_dump(round(Input::get("comprometido"),0))   ;
+                    var_dump(round(Input::get("comprometido"),2))   ;
                     exit();
                     
                 return Response::json(array(
