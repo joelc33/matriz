30b1c91a4aa17a79a15b234d6304fb24020f5295
ISILI <ISILI@isilio>
2024-10-23 16:05:30 -0400

    cambios varios

--- app/Http/Controllers/AcSeguimiento/formatresController.php
+++ app/Http/Controllers/AcSeguimiento/formatresController.php
@@ -1303,7 +1303,7 @@ class formatresController extends Controller
                 'id_tab_meta_fisica',
                 'ac_seguimiento.tab_meta_financiera.id_tab_municipio_detalle',
                 'ac_seguimiento.tab_meta_financiera.id_tab_parroquia_detalle',
-                DB::raw('coalesce(mo_presupuesto,0) + coalesce(mo_modificado_anual,0) as mo_presupuesto'),
+                DB::raw('coalesce(mo_presupuesto,0) + coalesce(mo_modificado_anual,0) + coalesce(mo_modificado,0) as mo_presupuesto'),
                 'co_partida',
                 'id_tab_fuente_financiamiento',
                 'ac_seguimiento.tab_meta_financiera.in_activo',
