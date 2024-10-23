be9f9ad2e64f60d637df9bfdf21efa566ef701da
ISILI <ISILI@isilio>
2024-10-23 15:34:02 -0400

    cambios varios

--- app/Http/Controllers/AcSeguimiento/formacincoController.php
+++ app/Http/Controllers/AcSeguimiento/formacincoController.php
@@ -192,26 +192,27 @@ class formacincoController extends Controller
 
 
             $data = tab_forma_005::select(
-                'id',
-                'id_tab_ac',
-                'pp_anual',
-                'tp_indicador',
-                'nb_indicador_gestion',
-                'de_valor_obtenido',
-                'de_valor_objetivo',
-                'de_valor_obtenido_acu',
-                'de_valor_objetivo_acu',                    
-                'nu_cumplimiento',
-                'de_indicador_descripcion',
-                'de_formula',
-                'in_005',
-                'de_observacion as de_observacion_005',
-                'id_usuario_solicita',
-                'id_usuario_procesa',
-                'id_tab_estatus',
-                'in_005 as in_bloquear_005'
+                'tab_forma_005.id',
+                'tab_forma_005.id_tab_ac',
+                't01.pp_anual',
+                'tab_forma_005.tp_indicador',
+                'tab_forma_005.nb_indicador_gestion',
+                'tab_forma_005.de_valor_obtenido',
+                'tab_forma_005.de_valor_objetivo',
+                'tab_forma_005.de_valor_obtenido_acu',
+                'tab_forma_005.de_valor_objetivo_acu',                    
+                'tab_forma_005.nu_cumplimiento',
+                'tab_forma_005.de_indicador_descripcion',
+                'tab_forma_005.de_formula',
+                'tab_forma_005.in_005',
+                'tab_forma_005.de_observacion as de_observacion_005',
+                'tab_forma_005.id_usuario_solicita',
+                'tab_forma_005.id_usuario_procesa',
+                'tab_forma_005.id_tab_estatus',
+                'tab_forma_005.in_005 as in_bloquear_005'
             )
-            ->where('id', '=', $id)
+            ->join('seguimiento.tab_ac as t01', 'ac_seguimiento.tab_forma_005.id_tab_ac', '=', 't01.id')
+            ->where('tab_forma_005.id', '=', $id)
             ->first();
 
         
