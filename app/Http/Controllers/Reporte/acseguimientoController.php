eac28192f475b1e86ede4b708a71f4731a34a284
ISILI <ISILI@isilio>
2025-02-02 12:44:25 -0400

    cambios varios

--- app/Http/Controllers/Reporte/acseguimientoController.php
+++ app/Http/Controllers/Reporte/acseguimientoController.php
@@ -2760,7 +2760,7 @@ foreach($actividad as $item) {
             ->orderBy('codigo', 'ASC')
             ->first();
             
-            $data12 = tab_meta_fisica::select('codigo','nb_meta','de_desvio'
+            $data12 = tab_meta_fisica::select('tab_meta_fisica.id','codigo','nb_meta','de_desvio'
             )
              ->join('ac_seguimiento.tab_ac_ae as t03', 'tab_meta_fisica.id_tab_ac_ae', '=', 't03.id')
              ->join('mantenimiento.tab_ac_ae_predefinida as t04', 't03.id_tab_ac_ae_predefinida', '=', 't04.id')
@@ -2776,8 +2776,9 @@ foreach($actividad as $item) {
              ->groupBy('codigo')
              ->groupBy('nb_meta')
             ->groupBy('de_desvio')
+            ->groupBy('tab_meta_fisica.id')        
             ->distinct()        
-            ->orderBy('codigo', 'ASC')
+            ->orderBy('tab_meta_fisica.id', 'ASC')
             ->get();            
     
             $tab_meta_financiera = tab_meta_fisica::select('codigo','nb_meta',
