0fffa382db77d0c26cb8f4cac653c23b404d8935
camarilloj <joelc33@gmail.com>
2026-07-07 17:22:27 -0400

    reporte

--- app/Http/Controllers/Reporte/acseguimientoController.php
+++ app/Http/Controllers/Reporte/acseguimientoController.php
@@ -3966,7 +3966,7 @@ $html1 = '
         </tr>';
 
                 $sqlLineaT = tab_ac_linea_transformacion::select('tx_transformacion')
-                    ->where('id_tab_ac', '=', $id)
+                    ->where('id_tab_ac', '=', $data->id)
                     ->groupBy('tx_transformacion')
                     ->orderBy('tx_transformacion', 'ASC')
                     ->get();
@@ -3985,7 +3985,7 @@ $html1 = '
 
 
                 $sqlLineaE = tab_ac_linea_transformacion::select('tx_eje_alineacion')
-                    ->where('id_tab_ac', '=', $id)
+                    ->where('id_tab_ac', '=', $data->id)
                     ->groupBy('tx_eje_alineacion')
                     ->orderBy('tx_eje_alineacion', 'ASC')
                     ->get();
@@ -4001,7 +4001,7 @@ $html1 = '
                 $html1 .= ' </table></td></tr>';
 
                 $sqlLineaI = tab_ac_linea_transformacion::select('tx_linea_impulso')
-                    ->where('id_tab_ac', '=', $id)
+                    ->where('id_tab_ac', '=', $data->id)
                     ->groupBy('tx_linea_impulso')
                     ->orderBy('tx_linea_impulso', 'ASC')
                     ->get();
@@ -4018,7 +4018,7 @@ $html1 = '
                 $html1 .= ' </table></td></tr>';
 
                 $sqlLineaF = tab_ac_linea_transformacion::select('tx_foco_accion')
-                    ->where('id_tab_ac', '=', $id)
+                    ->where('id_tab_ac', '=', $data->id)
                     ->groupBy('tx_foco_accion')
                     ->orderBy('tx_foco_accion', 'ASC')
                     ->get();
