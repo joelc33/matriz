373cfc48e127690f9a362bcd1327dbaa95d0b1a3
camarilloj <joelc33@gmail.com>
2026-07-14 12:49:54 -0400

    matriz

--- app/Http/Controllers/Reporte/acseguimientoController.php
+++ app/Http/Controllers/Reporte/acseguimientoController.php
@@ -444,7 +444,7 @@ class acseguimientoController extends Controller
           'ac_seguimiento.tab_ac.tx_pr_programado',
           'ac_seguimiento.tab_ac.tx_pr_obtenido',
           'id_tab_tipo_periodo',
-          'ac_seguimiento.tab_ac.de_observacion_002',
+          'ac_seguimiento.tab_ac.de_observacion_003',
           'ac_seguimiento.tab_ac.de_observacion_002',
           'ac_seguimiento.tab_ac.de_sector',
           'ac_seguimiento.tab_ac.id',
